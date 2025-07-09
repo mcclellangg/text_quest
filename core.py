ea6e6f2d055377fd95eacb569f70b2c43b81c589
Grayson <mcclellangg@gmail.com>
2025-07-09 16:16:06 -0400

    update core to support live state representation of game objects; add room_map to GameCoordinator; remove reference to game_data in save_game_to_file and update it to use get_game_state

--- core.py
+++ core.py
@@ -26,6 +26,7 @@ class GameCoordinator:
         # Game State
         self.player = Player.from_dict(self.game_data["player"])
         self.current_room = Room.from_dict(self.game_data["rooms"]["start_room"])
+        self.room_map = self.load_game_rooms()
         self.item_map = self.load_game_items()
 
         self.logger.info("GameCoordinator initialized.")
@@ -99,11 +100,11 @@ class GameCoordinator:
 
     def load_game_items(self):
         """
-        Reads 'items' in from game_data and creates a map in below format so that 
+        Reads 'items' in from game_data and creates a map in below format so that
         items objects can be accessed via keyword:
         {
-         'item_id': Item,
-         'item_id_lamp': Lamp Item
+         'item_id_A': Item_A,
+         'item_id_B': Item_B
         }
         """
         game_items = deepcopy(self.game_data["items"])
@@ -111,16 +112,32 @@ class GameCoordinator:
             item_id: Item.from_dict(item_dict)
             for item_id, item_dict in game_items.items()
         }
-    
+
+    def load_game_rooms(self):
+        """
+        Reads 'rooms' in from game_data and creates a map in below format so that
+        rooms objects can be accessed via keyword:
+        {
+         'room_id_A: Room_A,
+         'room_id_B: Room_B,
+        }
+        """
+        game_rooms = deepcopy(self.game_data["rooms"])
+        return {
+            room_id: Room.from_dict(room_dict)
+            for room_id, room_dict in game_rooms.items()
+        }
+
     def convert_item_map_to_dict(self):
         return {item_id: item.to_dict() for item_id, item in self.item_map.items()}
 
     def post_load_game_file_processing(self):
-        "Update game state (item_map, player, current_room), and display current room"
+        "Generate live state for objects from loaded game_data, should be called anytime game is loaded/restarted."
         self.player = Player.from_dict(self.game_data["player"])
         current_room_id = self.player.get_current_location()
         self.current_room = Room.from_dict(self.game_data["rooms"][current_room_id])
         self.item_map = self.load_game_items()
+        self.room_map = self.load_game_rooms()
         self.current_room.display_room()
 
     def handle_restart(self, args):
@@ -143,6 +160,17 @@ class GameCoordinator:
         else:
             return self.save_game_to_file()
 
+    def get_game_state(self):
+        return {
+            "items": {
+                item_id: item.to_dict() for item_id, item in self.item_map.items()
+            },
+            "player": self.player.to_dict(),
+            "rooms": {
+                room_id: room.to_dict() for room_id, room in self.room_map.items()
+            },
+        }
+
     def save_game_to_file(
         self, filename: str = "PROT01", dir: str = "save_files"
     ) -> str:
@@ -150,7 +178,8 @@ class GameCoordinator:
 
         try:
             with open(file_path, mode="w", encoding="utf-8") as f:
-                json.dump(self.game_data, f, indent=4, sort_keys=True)
+                game_state = self.get_game_state()
+                json.dump(game_state, f, indent=4, sort_keys=True)
                 self.logger.info(f"Game save: {file_path}")
                 return file_path
         except Exception as e:
@@ -196,45 +225,54 @@ class GameCoordinator:
                 valid_target_item = self.item_map[target]
             except KeyError as e:
                 self.logger.info(f"Unknown item: {valid_target_item}")
-            if valid_target_item and (valid_target_item.get_current_location() == self.player.get_current_location()):
+            if valid_target_item and (
+                valid_target_item.get_current_location()
+                == self.player.get_current_location()
+            ):
                 # STATE CHANGE #
-                self.player.add_item_to_inventory(valid_target_item) # This just adds id of target
-                self.item_map[valid_target_item.id].update_current_location("player_inventory")
+                self.player.add_item_to_inventory(
+                    valid_target_item
+                )  # This just adds id of target
+                self.item_map[valid_target_item.id].update_current_location(
+                    "player_inventory"
+                )
                 # call for game data update
                 # NOTE: may be desirable to move this to higher level function (so player_moves can be counted in post_processing.)
-                self.game_data = self.update_game_data()
+                # self.game_data = self.update_game_data()
             elif valid_target_item:
-                print(f"No {valid_target_item.name} here, why don't you look somewhere else.")
+                print(
+                    f"No {valid_target_item.name} here, why don't you look somewhere else."
+                )
             else:
                 print(f"Can't take that: {target}.")
         else:
             self.logger.error(f"ERROR-Unexpected args passed: {args}")
-    
-    def update_game_data(self):
-        """
-        Called after state change events to write changes in Game State (player, item_map, current_room)
-        to game_data.
 
-        NOTE: currently this assumes changes can only be made in the current_room, so only those will
-        need to be maintained.
-        """
-        next_game_data = {}
-        next_game_data["rooms"] = deepcopy(self.game_data["rooms"])
-        next_game_data["items"] = deepcopy(self.game_data["items"])
-        
-        # Will need to add info from current room, as this is not handled by the room itself
-        # num_visits are calculated when rooms are initially entered, but that is not logged into the game_state
-        current_room_id = self.current_room.get_id()
-        next_game_data["rooms"][current_room_id] = self.current_room.to_dict()
+    # def update_game_data(self):
+    #     """
+    #     Called after state change events to write changes in Game State (player, item_map, current_room)
+    #     to game_data.
 
-        # Update player info
-        next_game_data["player"] = self.player.to_dict()
+    #     NOTE: currently this assumes changes can only be made in the current_room, so only those will
+    #     need to be maintained.
+    #     """
+    #     next_game_data = {}
+    #     next_game_data["rooms"] = deepcopy(self.game_data["rooms"])
+    #     next_game_data["items"] = deepcopy(self.game_data["items"])
 
-        # Update items
-        item_map_dict = self.convert_item_map_to_dict()
-        next_game_data["items"] = item_map_dict
+    #     # Will need to add info from current room, as this is not handled by the room itself
+    #     # num_visits are calculated when rooms are initially entered, but that is not logged into the game_state
+    #     current_room_id = self.current_room.get_id()
+    #     next_game_data["rooms"][current_room_id] = self.current_room.to_dict()
 
-        return next_game_data
+    #     # Update player info
+    #     next_game_data["player"] = self.player.to_dict()
+
+    #     # Update items
+    #     item_map_dict = self.convert_item_map_to_dict()
+    #     next_game_data["items"] = item_map_dict
+
+    #     return next_game_data
 
     # Run game
     def run_game(self):
